e603132c770a772e7a8c6e64ce4bf4408b7a5f36
Max Bromberg <maxwell.bromberg@gmail.com>
2019-08-04 16:58:45 -0500

    Hotfix 0.0.2

--- events/message.js
+++ events/message.js
@@ -11,7 +11,7 @@ module.exports = (client, message) => {
   if (message.author.bot) return
 
   if (message.channel.type === 'dm') {
-    new Discord.RichEmbed()
+    var embed = new Discord.RichEmbed()
       .setTitle(`**Mod Mail is Coming Soon!**`)
       .setDescription(`In the meantime, you can look for a moderator in <&420594746990526468>!`)
       .setColor('#00b3b3')
@@ -20,7 +20,7 @@ module.exports = (client, message) => {
     return message.channel.send(embed)
   }
 
-  if (message.content.startsWith('```') && message.content.split('\n', 1)[0].includes('```') && message.content.endsWith('```')) {
+  if (message.content.startsWith('```') && (message.content.split('\n', 20).length > 15) && message.content.split('\n', 1)[0].includes('```') && message.content.endsWith('```')) {
     let embed = new Discord.RichEmbed()
       .setTitle('Code block detected! Automagically shooting to GitHub...')
       .setDescription('The link will appear right here in just a moment.')
@@ -49,12 +49,19 @@ module.exports = (client, message) => {
 
     message.channel.send(embed).then(m => {
       gists.create(gistObject).then(gist => {
-        embed.setTitle('Code block detected! Automagically shooting to GitHub...')
+        embed.setTitle('Code block pasted to github! Removing the original message...')
         embed.setDescription(`**${gist.body.html_url}**`)
         embed.setColor('#00b3b3')
         embed.setFooter(client.footer, icon)
         embed.setTimestamp(new Date())
         m.edit(embed)
+
+        message.delete().then(() => {
+          embed.setTitle('Code block pasted to github! The original message is deleted, you can access the code with the link below:')
+          return m.edit(embed)
+        }).catch(err => {
+          return console.log(err)
+        })
       })
     }).catch(err => {
       console.error(err)
@@ -64,7 +71,7 @@ module.exports = (client, message) => {
       embed.setColor('#00b3b3')
       embed.setFooter(client.footer, icon)
       embed.setTimestamp(new Date())
-      m.edit(embed)
+      return m.edit(embed)
     })
   }
 
