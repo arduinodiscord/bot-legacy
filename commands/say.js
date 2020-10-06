c7e3b7e7fe559891b0872390691d5f9b8200972e
BluLightShow <20815595+BluLightShow@users.noreply.github.com>
2020-10-06 16:17:11 -0500

    feat: say command complete

--- commands/say.js
+++ commands/say.js
@@ -68,7 +68,7 @@ class SayCommand extends Command {
     if ((args.fields.length % 2) !== 0) {
       return message.channel.send(
         new MessageEmbed(embed)
-          .setTitle('No value was supplied for your final field. Every field must have a name and a value. Cancelling command.')
+          .setTitle('No value was supplied for your final field. Every field must have a name and a value. Canceling command.')
           .setTimestamp(new Date())
       )
     }
@@ -79,21 +79,22 @@ class SayCommand extends Command {
     ).then(m => {
       m.react('✅').then(() => {
         m.react('❌').then(() => {
-          m.awaitReactions(reaction => reaction.users.resolve(message.author), { time: 300000 }).then(reaction => {
-            console.log('first test ' + reaction.emoji.toString())
+          m.awaitReactions((reaction, user) => user === message.author, { max: 1, time: 300000 }).then(reactionCollection => {
+            let reaction = reactionCollection.first()
             if (reaction.emoji.toString() === '✅') {
               message.channel.send(
                 new MessageEmbed(embed)
                   .setTitle('What thumbnail would you like to add? Please provide a direct URL.')
                   .setTimestamp(new Date())
               ).then(() => {
-                message.channel.awaitMessages(msg => msg.author.id === message.author.id).then(msg => {
+                message.channel.awaitMessages(msg => msg.author === message.author, { max: 1, time: 300000}).then(msgCollector => {
+                  let msg = msgCollector.first()
                   try {
                     var url = new URL(msg.content)
                   } catch (err) {
                     return message.channel.send(
-                      new MessageEmbed()
-                        .setTitle('URL provided was invalid. Cancelling command.')
+                      new MessageEmbed(embed)
+                        .setTitle('URL provided was invalid. Canceling command.')
                         .setTimestamp(new Date())
                     )
                   }
@@ -102,28 +103,44 @@ class SayCommand extends Command {
                 })
               })
             } else if (reaction.emoji.toString() === '❌') {
-              console.log('second one')
               sendIt(false)
             }
           })
         })
       })
-      console.log('test')
     })
 
     function sendIt(thumbnail) {
       const say = new MessageEmbed(embed)
         .setTimestamp(new Date())
         .setTitle(args.title).setDescription(args.description)
-      for (let i = 1; i <= args.fields.length / 2; i++) {
-        let name = args.shift()
-        let value = args.shift()
+
+      // And that's on spending an hour realizing you can't iterate over an array you're modifying
+      const iterableLength = args.fields.length
+
+      for (let i = 0; i < iterableLength / 2; i++) {
+        let name = args.fields.shift()
+        let value = args.fields.shift()
         say.addField(name, value)
       }
       if (thumbnail) {
         say.setThumbnail(args.thumbnail)
       }
-      message.guild.channels.resolve(args.channel).send(say)
+      message.guild.channels.resolve(args.channel).send(say).then(() => {
+        message.channel.send(
+          new MessageEmbed(embed)
+          .setTimestamp(new Date())
+          .setTitle('Great success!')
+          .setDescription('Embed sent.')
+        )
+      }).catch(err => {
+        message.channel.send(
+          new MessageEmbed(embed)
+          .setTimestamp(new Date())
+          .setTitle('An error occured whilst trying to send.')
+          .setDescription('Error: ' + err)
+        )
+      })
     }
   }
 }
