cb37e810e894d727a61e554afff4288c71e25bb2
BluLightShow <20815595+BluLightShow@users.noreply.github.com>
2020-12-24 14:56:29 -0600

    feat: arduino employees bypass file filter
    Signed-off-by: BluLightShow <20815595+BluLightShow@users.noreply.github.com>

--- listeners/message.js
+++ listeners/message.js
@@ -15,7 +15,7 @@ class MessageListener extends Listener {
     if (message.attachments.find(attachment => {
       const allowedExtensions = ['png', 'jpg', 'gif', 'webp', 'tiff', 'heif', 'jpeg', 'svg', 'webm', 'mpg', 'mpeg', 'ogg', 'mp4', 'm4v', 'avi', 'mov', 'm4a', 'mp3', 'wav']
 	    const extension = attachment.name.split('.').pop().toLowerCase()
-	    return !allowedExtensions.includes(extension)
+	    return (!allowedExtensions.includes(extension)) && (!message.member.roles.cache.find(role => role.id === '451152561735467018'))
     })) {
       message.delete().then(() => {
         message.channel.send(
