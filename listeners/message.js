6c1a6449948ed738e040f204d9965a666060c8dc
Elias Hernandez <ehernandez@evistaresources.com>
2021-01-19 14:37:52 -0500

    Added .pdf to the list of allowed extensions.

--- listeners/message.js
+++ listeners/message.js
@@ -14,7 +14,7 @@ class MessageListener extends Listener {
   exec(message) {
     // File filter
     if (message.attachments.find(attachment => {
-      const allowedExtensions = ['png', 'jpg', 'gif', 'webp', 'tiff', 'heif', 'jpeg', 'svg', 'webm', 'mpg', 'mpeg', 'ogg', 'mp4', 'm4v', 'avi', 'mov', 'm4a', 'mp3', 'wav']
+      const allowedExtensions = ['png', 'jpg', 'gif', 'webp', 'tiff', 'heif', 'jpeg', 'svg', 'webm', 'mpg', 'mpeg', 'ogg', 'mp4', 'm4v', 'avi', 'mov', 'm4a', 'mp3', 'wav', 'pdf']
 	    const extension = attachment.name.split('.').pop().toLowerCase()
 	    return (!allowedExtensions.includes(extension)) && (!message.member.roles.cache.find(role => role.id === '451152561735467018'))
     })) {
