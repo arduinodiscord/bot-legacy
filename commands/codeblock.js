2c81d45f9ac19de99d2486a187827c4080469630
tasdogu <73469143+tasdogu@users.noreply.github.com>
2021-02-25 08:31:59 +0300

    Update codeblock.js

--- commands/codeblock.js
+++ commands/codeblock.js
@@ -16,9 +16,9 @@ class CodeBlockCommand extends Command {
       new MessageEmbed(embed)
       .setTimestamp(new Date())
       .setTitle('How to Send Code Blocks')
-      .setImage('https://cdn.discordapp.com/attachments/747642578668748800/754151197425795202/unknown.png')
-      .setDescription('Surround the code in three backticks. If using new lines, a file extension can be placed directly after the first 3 backticks to highlight in that language. An example is shown below, highlighting code in C++.')
+      .setImage('https://cdn.discordapp.com/attachments/758046379133239407/813767505000398929/rllpwo.png')
+      .setDescription('Surround the code in three backticks. If using new lines, a file extension can be placed directly after the first 3 backticks to highlight in that language. An example is shown below, highlighting code in Arduino syntax. The backtick key is typically found to the left of the 1 key.')
     )
   }
 }
-module.exports = CodeBlockCommand
+module.exports = 
